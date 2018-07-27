e3781e5b7d9bf2649d762c3311ad36a58497ff64
Saulo Vinicius <sauloviniciusf@Saulos-MacBook-Pro.local>
2018-07-27 08:37:14 -0300

    Criado component que lista livros em suas respectivas estates quote> Estantes montadas buscando livros da API disponibilizada

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <div className="app-header">
           <div className="app-header__title">
             My Reads
+          </div>
+        </div>
+        <div className="list-books">
+          <div className="list-books__container">
+            {/* Shelf - Currently Reading */}
+            <ListBooks
+              shelfTitle="Currently Reading"
+              books={this.state.books.filter(books => books.shelf === 'currentlyReading')}
+            />
+            {/* Shelf - Want to Read */}
+            <ListBooks
+              shelfTitle="Want to Read"
+              books={this.state.books.filter(books => books.shelf === 'wantToRead')}
+            />
+            {/* Shelf - Read */}
+            <ListBooks
+              shelfTitle="Read"
+              books={this.state.books.filter(books => books.shelf === 'read')}
+            />
           </div>
         </div>
       </div>
